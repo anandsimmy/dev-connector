0fbdd2d0cda0bf31b1b4a7db041acb16c289ab0b
Anand Simmy <anandsimmy7@gmail.com>
2020-10-21 08:28:28 +0530

    added routes to server

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const { connect } = require('mongoose');
 const connectDB= require('./config/db');
 
 const app= express();
+
+//Connecting Database
 connectDB();
 
 app.get('/', (req, res)=>{
     res.send('API is running');
 })
+
+//Defining Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT= process.env.PORT || 5000;
 
