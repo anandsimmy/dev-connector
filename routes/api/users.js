e64d329217eb755d1681f2e177fee5d316e22a32
Anand Simmy <anandsimmy7@gmail.com>
2020-10-22 08:46:30 +0530

    added post routes for registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,20 @@
 const express= require('express');
-
 const router= express.Router();
+const { body, validationResult }= require('express-validator');
 
-// @route GET api/users
+// @route POST api/users
+// @desc register users
 // @access public
-router.get('/', (req, res)=> {
+
+router.post('/', [
+    body('name', 'Please enter a name').not().isEmpty(),
+    body('email', 'Please enter a valid email').isEmail(),
+    body('password', 'Please enter a password with min 6 characters').isLength({ min:6 })
+], (req, res)=> {
+    const errors= validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
     res.send('User route')
 });
 
