d419461db0c83c2cf30a3bb66c65418d27875cd2
Anand Simmy <anandsimmy7@gmail.com>
2020-10-25 13:25:42 +0530

    added jwt sign

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,7 +47,14 @@ router.post('/', [
                 id: user.id
             }
         }
-        
+        jwt.sign(
+            payload, 
+            config.get('jwtToken'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if(err) throw err
+                res.json({ token })
+            })
     }catch(err){
         console.log(err.message)
         res.status(500).send('Server Error')
