2fb12c9916706c49b309ae0163112d73db1a7aeb
Anand Simmy <anandsimmy7@gmail.com>
2020-10-27 14:45:35 +0530

    implemented auth check

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
 const express= require('express');
+const auth= require('../../middleware/auth');
+const User= require('../../models/User');
 
 const router= express.Router();
 
 // @route GET api/auth
 // @access public
-router.get('/', (req, res)=> {
-    res.send('Auth route')
+
+router.get('/', auth, async (req, res)=> {
+    try{
+        const user= await User.findById(req.user).select('-password');
+        res.send(user)
+    }catch(err){
+        console.error(err.message)
+        return res.status(500).send('Server Error')
+    }
 });
 
 module.exports= router;
