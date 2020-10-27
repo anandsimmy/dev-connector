5cd5563ca2ca05dba75340abe5c2d698571ba13c
Anand Simmy <anandsimmy7@gmail.com>
2020-10-27 14:45:49 +0530

    added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt= require('jsonwebtoken');
+const config= require('config');
+
+module.exports= function(req, res, next) {
+    //get token from header
+    const token= req.header('x-auth-token')
+
+    //verify token
+    if(!token){
+        return res.status(401).json({ msg: 'No token, access denied!' })
+    }
+    try{
+        const decoded= jwt.verify(token, config.get('jwtSecret'))
+        req.user= decoded.user.id
+        next()
+    }catch(err){
+        console.log(err.message)
+        return res.status(401).json({ msg: 'Invalid token, access denied' })
+    }
+}
