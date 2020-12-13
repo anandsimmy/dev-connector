0fa8fab3f48f9c68ff45cfe3e26943f5c4086354
Anand Simmy <anandsimmy7@gmail.com>
2020-12-13 10:29:34 +0530

    added comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -131,4 +131,69 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 })
 
+// @route POST api/posts/comment/:id
+// @desc Comment on a post
+// @access private
+router.post('/comment/:id', [auth,
+    [body('text', 'Text is required').not().isEmpty()]
+], async (req, res) => {
+    try {
+        const errors= validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const user= await User.findById(req.user.id)
+        const post= await Post.findById(req.params.id)
+
+        const newComment= {
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id,
+            text: req.body.text
+        }
+
+        post.comments.unshift(newComment)
+        await post.save()
+
+        res.json(post.comments)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Delete comment on a post
+// @access private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post= await Post.findById(req.params.id)
+        
+        //find comment to delete
+        const commentToDelete= post.comments.find(comment => comment.id.toString() === req.params.comment_id)
+        
+        //check comment exists
+        if(!commentToDelete){
+            return res.status(404).json({ msg: 'Comment not found' })
+        }
+
+        //check if user is authorized to delete the comment
+        if(commentToDelete.user.toString() !== req.user.id){
+            return res.status(401).json({ msg: 'User not authorized' })
+        }
+
+        //find comment index
+        const removeIndex= post.comments.map(comment => comment.id.toString()).indexOf(req.params.comment_id)
+
+        //delete comment
+        post.comments.splice(removeIndex, 1)
+
+        await post.save()
+        res.json(post.comments)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports= router;
