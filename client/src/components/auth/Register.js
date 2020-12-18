36956f73ce5e0a72d12ad8d49fefdb4deea79b6d
Anand Simmy <anandsimmy7@gmail.com>
2020-12-18 18:20:57 +0530

    added login and register forms

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register= () => {
 
@@ -76,7 +77,7 @@ const Register= () => {
                 <input type="submit" className="btn btn-primary" value="Register" />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to='/login'>Sign In</Link>
             </p>
         </>
     )
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Landing= () => {
     return (
-        <section class="landing">
-            <div class="dark-overlay">
-                <div class="landing-inner">
-                <h1 class="x-large">Developer Connector</h1>
-                <p class="lead">
+        <section className="landing">
+            <div className="dark-overlay">
+                <div className="landing-inner">
+                <h1 className="x-large">Developer Connector</h1>
+                <p className="lead">
                     Create a developer profile/portfolio, share posts and get help from
                     other developers
                 </p>
-                <div class="buttons">
+                <div className="buttons">
                     <Link to='/register' className='btn btn-primary'>Sign Up</Link>
                     <Link to='/login' className='btn btn-light'>Login</Link>
                 </div>
