374ea0e9144d9057429ca0897d208e6551fc7974
Anand Simmy <anandsimmy7@gmail.com>
2021-01-01 08:09:46 +0530

    added auth action

---
+++ client/src/actions/auth.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import { 
+    REGISTER_SUCCESS,
+    REGISTER_FAIL } from './types'
+import { setAlert } from './alert'
+
+export const registerUser= ({ name, email, password }) => async (dispatch) => {
+    const config= {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body= JSON.stringify({ name, email, password })
+
+    try{
+        const res= await axios.post('/api/users', body, config)
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        })
+
+    }catch(err){
+        const errors= err.response.data.errors
+        errors.forEach(error => dispatch(setAlert(error.msg), 'danger'))
+        dispatch({
+            type: REGISTER_FAIL
+        })
+    }
+}
