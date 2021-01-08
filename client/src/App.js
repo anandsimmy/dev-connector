23189d907cae04e6fe0bc56bc419cce9a719a2d3
Anand Simmy <anandsimmy7@gmail.com>
2021-01-08 08:02:29 +0530

    added user loading at root level

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Alert from './components/layout/Alert'
+import setAuthToken from './utils/setAuthToken'
+import { loadUser } from './actions/auth'
 import './App.css';
 
 //redux
 import { Provider } from 'react-redux'
 import store from './store'
 
-const App= () => (
+if(localStorage.token){
+  setAuthToken(localStorage.token)
+}
+
+const App= () => {
+
+  useEffect(()=>{
+    store.dispatch(loadUser())
+  }, [])
+
+  return (
   <Provider store={store}>
     <Router>
       <Navbar />
@@ -25,6 +37,6 @@ const App= () => (
       </section>
     </Router>
   </Provider>
-)
+)}
 
 export default App;
