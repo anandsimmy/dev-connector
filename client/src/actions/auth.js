1510e5a8c4ecac9e18cade47feab7d2bcd833b18
Anand Simmy <anandsimmy7@gmail.com>
2021-01-08 08:01:47 +0530

    added loaduser action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,8 +1,30 @@
 import axios from 'axios'
 import { 
     REGISTER_SUCCESS,
-    REGISTER_FAIL } from './types'
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
+ } from './types'
 import { setAlert } from './alert'
+import setAuthToken from '../utils/setAuthToken'
+
+export const loadUser= () => async dispatch => {
+    if(localStorage.token){
+        setAuthToken(localStorage.token)
+    }
+    try {
+        const res= await axios.get('/api/auth')
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        })
+    } catch (err) {
+        console.log(err.message)
+        dispatch({
+            type: AUTH_ERROR
+        })
+    }
+}
 
 export const registerUser= ({ name, email, password }) => async (dispatch) => {
     const config= {
