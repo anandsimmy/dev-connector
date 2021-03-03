920cda4b1187168a1fae7a14220e114091ded7c9
Anand Simmy <anandsimmy7@gmail.com>
2021-03-03 09:07:45 +0530

    added createProfile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
+import { setAlert } from './alert'
 import { GET_PROFILE, PROFILE_ERROR } from './types'
 
+//get profile data
 export const getCurrentProfile=() => async (dispatch) => {
        try {
             const res= await axios.get('/api/profile/me')
@@ -14,4 +16,35 @@ export const getCurrentProfile=() => async (dispatch) => {
                 payload: { msg: err.response.data.msg, status: err.response.status }
             })
        } 
+}
+
+//create profile
+export const createProfile= (formData, history, edit=false) => async dispatch => {
+    try {
+        const config= {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res= await axios.post('/api/profile/', formData, config)
+
+        dispatch({
+            type: GET_PROFILE, 
+            payload: res.data
+        })
+
+        dispatch(setAlert(edit ? 'Profile Update': 'Profile Created', 'success'))
+
+        if(!edit){
+            history.push('/dashboard')
+        }
+    } catch (err) {
+        const errors= err.response?.data?.errors
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        dispatch({
+            type: PROFILE_ERROR, 
+            payload: { msg: err.response.data.msg, status: err.response.status }
+        })
+    }
 }
