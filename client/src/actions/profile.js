af38ecafc0025984bd9e108502e768f0e3dfff75
Anand Simmy <anandsimmy7@gmail.com>
2021-03-09 07:53:12 +0530

    added update profile actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { GET_PROFILE, PROFILE_ERROR } from './types'
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types'
 
 //get profile data
 export const getCurrentProfile=() => async (dispatch) => {
@@ -39,6 +39,64 @@ export const createProfile= (formData, history, edit=false) => async dispatch =>
         if(!edit){
             history.push('/dashboard')
         }
+    } catch (err) {
+        const errors= err.response?.data?.errors
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        dispatch({
+            type: PROFILE_ERROR, 
+            payload: { msg: err.response.data.msg, status: err.response.status }
+        })
+    }
+}
+
+//add experience
+export const addExperience= () => async dispatch => {
+    try {
+        const config= {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res= await axios.put('/api/profile/experience', formData, config)
+
+        dispatch({
+            type: UPDATE_PROFILE, 
+            payload: res.data
+        })
+
+        dispatch(setAlert('Experience added successfully', 'success'))
+
+        history.push('/dashboard')
+    } catch (err) {
+        const errors= err.response?.data?.errors
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        dispatch({
+            type: PROFILE_ERROR, 
+            payload: { msg: err.response.data.msg, status: err.response.status }
+        })
+    }
+}
+
+//add experience
+export const addEducation= () => async dispatch => {
+    try {
+        const config= {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res= await axios.put('/api/profile/education', formData, config)
+
+        dispatch({
+            type: UPDATE_PROFILE, 
+            payload: res.data
+        })
+
+        dispatch(setAlert('Education added successfully', 'success'))
+
+        history.push('/dashboard')
     } catch (err) {
         const errors= err.response?.data?.errors
         errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
