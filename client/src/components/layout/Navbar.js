0a7104bb835cf9b3de437ef2d38b780a1141f798
Anand Simmy <anandsimmy7@gmail.com>
2021-04-26 09:02:18 +0530

    minor navbar changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,6 +9,11 @@ const Navbar= ({ auth: { isAuthenticated, loading }, logoutUser }) => {
     const authLinks= (
     <ul>
         <li>
+            <Link to='/profiles'>
+                Developers
+            </Link>
+        </li>
+        <li>
             <Link to='/dashboard'>
                 <i className="fas fa-user"></i>{' '}
                 <span className='hide-sm'>Dashboard</span>
@@ -26,7 +31,7 @@ const Navbar= ({ auth: { isAuthenticated, loading }, logoutUser }) => {
     const guestLinks= (
         <ul>
             <li>
-                <Link to='#!'>Developers</Link>
+                <Link to='/profiles'>Developers</Link>
             </li>
             <li>
                 <Link to='/register'>Register</Link>
