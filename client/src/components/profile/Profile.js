81845a638ccdcdaa348c784d3435050a93534341
Anand Simmy <anandsimmy7@gmail.com>
2021-05-01 21:38:32 +0530

    added profile component

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+function Profile(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Profile.propTypes = {
+
+}
+
+export default Profile
+
