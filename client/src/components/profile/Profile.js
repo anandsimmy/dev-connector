7eb5f827e34c1dc11d3e25049444c3961d8729aa
Anand Simmy <anandsimmy7@gmail.com>
2021-05-02 09:43:43 +0530

    profile data fetching added

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { getProfileById } from '../../actions/profile'
+import Spinner from '../layout/Spinner'
+import { Link } from 'react-router-dom'
 
-function Profile(props) {
+const Profile= ({ match, profile: { profile, loading }, auth, getProfileById }) => {
+
+    useEffect(() => {
+        getProfileById(match.params.id)
+    }, [getProfileById])
+
     return (
-        <div>
-            
-        </div>
+        <>
+            {
+                (profile === null) ? <Spinner /> : (
+                    <>
+                        <Link to='/profiles' className='btn btn-light'>Back to Profiles</Link>
+                        {(auth.isAuthenticated && !auth.loading && auth.user._id === profile.user) && (
+                            <Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>
+                        ) }
+                    </>
+                )
+            }
+        </>
     )
 }
 
 Profile.propTypes = {
-
+    getProfileById: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
 }
 
-export default Profile
+const mapStateToProps= state => ({
+    profile: state.profile,
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { getProfileById })(Profile)
 
