07d3f6b50a09b30e5e6f9ca6ada80c49934a5bd3
Anand Simmy <anandsimmy7@gmail.com>
2021-05-03 09:29:15 +0530

    fixed security bug

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -11,6 +11,9 @@ export const getCurrentProfile=() => async (dispatch) => {
                 payload: res.data
             })
        } catch (err) {
+
+            dispatch({ type: CLEAR_PROFILE })
+
             dispatch({
                 type: PROFILE_ERROR, 
                 payload: { msg: err.response.data.msg, status: err.response.status }
@@ -48,7 +51,7 @@ export const getProfileById= userId => async (dispatch) => {
     } catch (err) {
          dispatch({
              type: PROFILE_ERROR, 
-             payload: { msg: err.response.data.msg, status: err.response.status }
+             payload: { msg: err?.response?.data?.msg, status: err?.response?.status }
          })
     } 
 }
