bf426311d16ecfbf8cbf6aa412406cfbee1fbe19
Anand Simmy <anandsimmy7@gmail.com>
2021-05-05 09:44:38 +0530

    added new route

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import CreateProfile from './components/profile-forms/CreateProfile'
 import EditProfile from './components/profile-forms/EditProfile'
 import AddExperience from './components/profile-forms/AddExperience'
 import AddEducation from './components/profile-forms/AddEducation'
+import Posts from './components/posts/Posts'
 import PrivateRoute from './components/routing/PrivateRoute'
 import setAuthToken from './utils/setAuthToken'
 import { loadUser } from './actions/auth'
@@ -43,6 +44,7 @@ const App= () => {
           <Route exact path='/register' component={Register} />
           <Route exact path='/profiles' component={Profiles} />
           <Route exact path='/profile/:id' component={Profile} />
+          <PrivateRoute exact path='/posts' component={Posts} />
           <PrivateRoute exact path='/dashboard' component={Dashboard} />
           <PrivateRoute exact path='/create-profile' component={CreateProfile} />
           <PrivateRoute exact path='/edit-profile' component={EditProfile} />
