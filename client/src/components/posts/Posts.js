c2b6f35485c5d25b4db0e988308fa05694b94b98
Anand Simmy <anandsimmy7@gmail.com>
2021-05-08 17:18:08 +0530

    added posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import Spinner from '../layout/Spinner'
 import { getPosts } from '../../actions/post'
+import PostItem from './PostItem'
 
 const Posts= ({ post: { posts, loading }, getPosts }) => {
 
@@ -13,7 +14,39 @@ const Posts= ({ post: { posts, loading }, getPosts }) => {
     return (
         <>
            {!loading ?  (
-               <>Posts fetched</>
+               <>
+                    <h1 className="large text-primary">
+                        Posts
+                    </h1>
+                    <p className="lead"><i className="fas fa-user"></i> Welcome to the community!</p>
+
+                    {/* <div className="post-form">
+                        <div className="bg-primary p">
+                        <h3>Say Something...</h3>
+                        </div>
+                        <form className="form my-1">
+                        <textarea
+                            name="text"
+                            cols="30"
+                            rows="5"
+                            placeholder="Create a post"
+                            required
+                        ></textarea>
+                        <input type="submit" className="btn btn-dark my-1" value="Submit" />
+                        </form>
+                    </div> */}
+
+                    <div className="posts">
+                        {
+                        posts.map((post) => {
+                            return (
+                                <PostItem key={post._id} post={post} />
+                            )
+                        })
+                    }
+                    
+                    </div>
+               </>
                ) : (<Spinner />)
            }
         </>
