9ffbb75818305813ca07b4756ef5afd1b48c5448
Anand Simmy <anandsimmy7@gmail.com>
2021-05-09 10:17:08 +0530

    populated profile api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -121,7 +121,10 @@ router.get('/', async (req, res) => {
 // @access public
 router.get('/user/:user_id', async (req, res) => {
     try{
-        const profile= await Profile.findOne({ user: req.params.user_id })
+        const profile= await Profile.findOne({ user: req.params.user_id }).populate(
+            'user',
+            [ 'name', 'avatar' ]
+        );
         if(!profile) return res.status(400).json({ msg: 'Profile not found' })
         res.json(profile)
     }catch(err){
@@ -285,7 +288,7 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', async (req, res) => {
     try {
         const options= {
-            uri: `https://api.github.com/users/${req.params.username}/repos?sort=created:asc&per_page=5
+            uri: `https://api.github.com/users/${req.params.username}/repos?sort=created:asc&per_page=10
             &client_id=${config.get('githubClientId')}&client_secret=${config.get('githubClientSecret')}`,
             method: 'GET',
             headers: { 'user-agent': 'node.js' }
