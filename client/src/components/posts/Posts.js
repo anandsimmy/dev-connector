f55a8fbf1ab33b64f0668acaa5a4536bf06225ad
Anand Simmy <anandsimmy7@gmail.com>
2021-05-13 09:23:02 +0530

    added separate component for newpost

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import NewPost from './NewPost'
+import PostItem from './PostItem'
 import Spinner from '../layout/Spinner'
 import { getPosts } from '../../actions/post'
-import PostItem from './PostItem'
 
 const Posts= ({ post: { posts, loading }, getPosts }) => {
 
@@ -13,38 +14,21 @@ const Posts= ({ post: { posts, loading }, getPosts }) => {
 
     return (
         <>
-           {!loading ?  (
+           {!loading && posts?.length > 0 ?  (
                <>
                     <h1 className="large text-primary">
                         Posts
                     </h1>
                     <p className="lead"><i className="fas fa-user"></i> Welcome to the community!</p>
-
-                    {/* <div className="post-form">
-                        <div className="bg-primary p">
-                        <h3>Say Something...</h3>
-                        </div>
-                        <form className="form my-1">
-                        <textarea
-                            name="text"
-                            cols="30"
-                            rows="5"
-                            placeholder="Create a post"
-                            required
-                        ></textarea>
-                        <input type="submit" className="btn btn-dark my-1" value="Submit" />
-                        </form>
-                    </div> */}
-
+                    <NewPost />
                     <div className="posts">
                         {
-                        posts.map((post) => {
-                            return (
-                                <PostItem key={post._id} post={post} />
-                            )
-                        })
-                    }
-                    
+                            posts.map((post) => {
+                                return (
+                                    <PostItem key={post._id} post={post} />
+                                )
+                            })
+                        }       
                     </div>
                </>
                ) : (<Spinner />)
