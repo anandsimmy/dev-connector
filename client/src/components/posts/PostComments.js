bec58eebd1343b05e5dc761e95b9b0c21b0a2f44
Anand Simmy <anandsimmy7@gmail.com>
2021-05-14 12:23:53 +0530

    added delete comment feature

--- client/src/components/posts/PostComments.js
+++ client/src/components/posts/PostComments.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 import Moment from 'react-moment'
 import PropTypes from 'prop-types'
 
-const PostComments= ({ comments }) => {
+const PostComments= ({ comments, auth }) => {
     return (
         <div className="comments">
         {
             comments?.length > 0 &&
                 comments.map(({ _id, name, avatar, user, text, date }) => (
-                    <div className="post bg-white p-1 my-1">
+                    <div key={_id} className="post bg-white p-1 my-1">
                     <div>
                         <Link to={`/profile/${user}`}>
                         <img
@@ -27,9 +27,13 @@ const PostComments= ({ comments }) => {
                         <p className="post-date">
                             Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
                         </p>
-                        <button type="button" className="btn btn-danger">
-                            <i className="fas fa-times"></i>
-                        </button>
+                        {
+                            auth.user?._id === user && (
+                                <button type="button" className="btn btn-danger">
+                                    <i className="fas fa-times"></i>
+                                </button>
+                            )
+                        }
                     </div>
                 </div>
                 ))
@@ -39,8 +43,14 @@ const PostComments= ({ comments }) => {
     )
 }
 
+PostComments.defaultProps= {
+    comments: [],
+    auth: {}
+}
+
 PostComments.propTypes = {
     comments: PropTypes.array.isRequired,
+    auth: PropTypes.object.isRequired,
 }
 
 export default PostComments
