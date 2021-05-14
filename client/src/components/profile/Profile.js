ec7e7ae1421010383a52707e3ce8e941d68a4f62
Anand Simmy <anandsimmy7@gmail.com>
2021-05-14 12:24:20 +0530

    fixed profile error

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -10,7 +10,7 @@ import { getProfileById } from '../../actions/profile'
 import Spinner from '../layout/Spinner'
 import { Link } from 'react-router-dom'
 
-const Profile= ({ match, profile: { profile, loading }, auth, getProfileById }) => {
+const Profile= ({ match, profile: { profile= null, loading }, auth, getProfileById }) => {
 
     useEffect(() => {
         getProfileById(match.params.id)
@@ -19,19 +19,25 @@ const Profile= ({ match, profile: { profile, loading }, auth, getProfileById }) 
     return (
         <>
             {
-                (profile === null) ? <Spinner /> : (
+                !profile ? <Spinner /> : (
                     <>
                         <Link to='/profiles' className='btn btn-light'>Back to Profiles</Link>
-                        {(auth.isAuthenticated && !auth.loading && auth.user._id === profile.user) && (
+                        {(auth.isAuthenticated && !auth.loading && auth.user._id === profile.user._id) && (
                             <Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>
                         )}
                         <div className='profile-grid my-1'>
-                            <ProfileTop profile={profile} />
-                            <ProfileAbout profile={profile} />
+                            {
+                                profile.user && (
+                                    <>
+                                        <ProfileTop profile={profile} />
+                                        <ProfileAbout profile={profile} />
+                                    </>
+                                )
+                            }
                             <div className="profile-exp bg-white p-2">
                                 <h2 className="text-primary">Experience</h2>
                                 {
-                                    profile.experience.length > 0 ? (
+                                    profile.experience?.length > 0 ? (
                                         profile.experience.map((exp, index) => {
                                             return <ProfileExperience key={index} experience={exp} />
                                         })
@@ -41,7 +47,7 @@ const Profile= ({ match, profile: { profile, loading }, auth, getProfileById }) 
                             <div className="profile-edu bg-white p-2">
                                 <h2 className="text-primary">Education</h2>
                                 {
-                                    profile.education.length > 0 ? (
+                                    profile.education?.length > 0 ? (
                                         profile.education.map((edu, index) => {
                                             return <ProfileEducation key={index} education={edu} />
                                         })
