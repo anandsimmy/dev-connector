a3a6dde0698aae563323b02fb49b2f5f45ae6d1b
Anand Simmy <anandsimmy7@gmail.com>
2021-05-14 12:23:22 +0530

    passing auth to comments component

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -1,20 +1,19 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
 import PostComments from './PostComments'
 import NewPostComment from './NewPostComment'
 import { getPostById } from '../../actions/post'
 import PropTypes from 'prop-types'
 
-const Post= ({ match: { params: { postId } }, getPostById, post: {
-    _id,
+const Post= ({ match: { params: { postId } }, auth, getPostById, post: {
     name,
     avatar,
     user, 
     text,
     date,
-    comments,
-    likes
+    comments
 } }) => {
 
     useEffect(() => {
@@ -26,7 +25,7 @@ const Post= ({ match: { params: { postId } }, getPostById, post: {
             <Link to='/posts' className="btn">Back To Posts</Link>
             <div className="post bg-white p-1 my-1">
                 <div>
-                    <Link to={`/profile/${user}}`}>
+                    <Link to={`/profile/${user}`}>
                         <img
                         className="round-img"
                         src={avatar}
@@ -36,27 +35,30 @@ const Post= ({ match: { params: { postId } }, getPostById, post: {
                     </Link>
                     </div>
                     <div>
-                    <p className="my-1">
-                        {text}
-                    </p>
+                    <p className="my-1">{text}</p>
+                    <p className="post-date">
+                            Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
+                        </p>
                 </div>
             </div>
 
             <NewPostComment />
 
-            <PostComments comments={comments} />
+            <PostComments comments={comments} auth={auth} />
         </>
     )
 }
 
 Post.propTypes = {
     match: PropTypes.object.isRequired,
-    getPostById: PropTypes.func.isRequired,
     post: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
+    getPostById: PropTypes.func.isRequired,
 }
 
 const mapStateToProps= state => ({
-    post: state.post.post
+    post: state.post.post,
+    auth: state.auth
 })
 
 export default connect(mapStateToProps, { getPostById })(Post)
