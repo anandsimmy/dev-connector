4ab4fe94e283c35004b9e9d699d410fb89d0e683
Anand Simmy <anandsimmy7@gmail.com>
2021-05-15 12:05:21 +0530

    added comment functionalities

--- client/src/components/posts/NewPostComment.js
+++ client/src/components/posts/NewPostComment.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { addComment } from '../../actions/post'
 
-const NewPostComment= () => {
+const NewPostComment= ({ addComment, postId }) => {
+
+    const [comment, setComment]= useState('')
+
+    const onSubmit= (e) => {
+        e.preventDefault()
+        addComment({
+            text: comment,
+            postId
+        })
+        setComment('')
+    }
+
     return (
         <div className="post-form">
             <div className="bg-primary p">
             <h3>Leave A Comment</h3>
             </div>
-            <form className="form my-1">
+            <form className="form my-1" onSubmit={onSubmit}>
             <textarea
                 name="text"
                 cols="30"
                 rows="5"
                 placeholder="Comment on this post"
+                value={comment}
+                onChange={e => setComment(e.target.value)}
                 required
             ></textarea>
             <input type="submit" className="btn btn-dark my-1" value="Submit" />
@@ -21,9 +37,14 @@ const NewPostComment= () => {
     )
 }
 
-NewPostComment.propTypes = {
-
+NewPostComment.defaultProps = {
+    postId: '',
+    addComment: () => {},
 }
 
-export default NewPostComment
+NewPostComment.propTypes = {
+    postId: PropTypes.string.isRequired,
+    addComment: PropTypes.func.isRequired,
+}
 
+export default connect(null, { addComment })(NewPostComment)
