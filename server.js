356242325ffd389f297092cb1b00424d0db3adac
Anand Simmy <anandsimmy7@gmail.com>
2021-05-22 09:18:43 +0530

    static folder added

--- server.js
+++ server.js
@@ -9,15 +9,24 @@ connectDB();
 //Initiating middlewares
 app.use(express.json()); //express validator
 
-app.get('/', (req, res)=>{
-    res.send('API is running');
-})
+// app.get('/', (req, res)=>{
+//     res.send('API is running');
+// })
 
 //Defining Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+//Serving static assets
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT= process.env.PORT || 5000;
 
